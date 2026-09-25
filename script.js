c119d161cb5bcbab28731b92ced37ee74b48199a
Aditya Mete <167237391+AdityaMete18@users.noreply.github.com>
2026-09-25 23:39:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name , age){
+		this.name = name;
+		this.age = age;
+	}
 
-class Student extends Person {}
+	get name(){
+		return "aditya"; 
+	}
+	set age(value){
+		this.age = value;
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	study(){
+		console.log(this.name , "is studing");
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(this.name,"is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
